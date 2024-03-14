4f7d8ffac3265b9ade9ad9947715c18ac28440ad
EvelinaSarg <126480724+EvelinaSarg@users.noreply.github.com>
2024-03-14 16:48:03 +0000

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -10,16 +10,16 @@ def get_details(poke_number):
 		url = f'https://pokeapi.co/api/v2/pokemon/{poke_number}/'
 		response = requests.get(url)
 		pokemon = response.json()
-		return pokemon['name'], pokemon['height'], pokemon['weight'], len(pokemon['moves'])
+		return pokemon['name'], pokemon['height'], pokemon['weight'], len(pokemon['moves']), pokemon['sprites']
 	except:
-		return 'Error', np.NAN, np.NAN, np.NAN
+		return 'Error', np.NAN, np.NAN, np.NAN, np.NAN
 	
 
 pokemon_number = st.slider("Pick a pokemon",
 						   min_value=1,
 						   max_value=150
 						   )
-name, height, weight, moves= get_details(pokemon_number)
+name, height, weight, moves, images= get_details(pokemon_number)
 height = height*10
 height_data=pd.DataFrame({'Pokemon': ['Weedle', name, 'victreebel'], 'Heights': [3, height, 17]})
 colours=['grey', 'blue', 'red']
@@ -29,8 +29,10 @@ graph = sns.barplot(data = height_data,
 				   y = 'Heights',
 				   palette = colours)
 
+
 st.write(f'Name: {name}')
 st.write(f'Height: {height}')
 st.write(f'Weight: {weight}')
 st.write(f'Move Count: {moves}')
+st.image(images['other']['home']['front_default'])
 st.pyplot(graph.figure)
