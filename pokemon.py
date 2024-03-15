03a278d3249816635b338d2dc09bb1e36a557770
EvelinaSarg <126480724+EvelinaSarg@users.noreply.github.com>
2024-03-15 11:39:45 +0000

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -4,14 +4,16 @@ import seaborn as sns
 import pandas as pd
 import requests
 import random
+from PIL import Image
 st.title("Pokemon Explorer!")
+
+
 def get_random_pokemon():
     poke_number = random.randint(1, 151)  # Generate a random Pokemon number between 1 and 150
     url = f'https://pokeapi.co/api/v2/pokemon/{poke_number}/'
     response = requests.get(url)
     pokemon = response.json()
     return pokemon['name'], pokemon['height'], pokemon['weight'], len(pokemon['moves']), pokemon['sprites'], pokemon['cries']
-
 
 def get_details(poke_number):
 	try:
@@ -20,25 +22,25 @@ def get_details(poke_number):
 		pokemon = response.json()
 		return pokemon['name'], pokemon['height'], pokemon['weight'], len(pokemon['moves']), pokemon['sprites'], pokemon['cries']
 	except:
-		return 'Error', np.NAN, np.NAN, np.NAN, np.NAN, np.NAN
+		return 'Error', np.NAN, np.NAN, np.NAN, np.NAN, npNAN
 	
 
 pokemon_number = st.slider("Pick a pokemon",
 						   min_value=1,
 						   max_value=150
 						   )
+
 name, height, weight, moves, images, sound= get_details(pokemon_number)
 if st.button('Show Random Pokemon'):
     name, height, weight, moves, images, sound = get_random_pokemon()
 height = height*10
 height_data=pd.DataFrame({'Pokemon': ['Weedle', name, 'victreebel'], 'Heights': [3, height, 17]})
-colours=['grey', 'blue', 'red']
+
 
 graph = sns.barplot(data = height_data,
 				   x= 'Pokemon',
 				   y = 'Heights',
 				   palette = colours)
-
 st.write(f'Name: {name}')
 st.write(f'Height: {height}')
 st.write(f'Weight: {weight}')
