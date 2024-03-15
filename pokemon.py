9995e775287b3beebb5fe2e5a0bba692ff4ff2ee
EvelinaSarg <126480724+EvelinaSarg@users.noreply.github.com>
2024-03-15 00:33:16 +0000

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -3,7 +3,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns 
 import pandas as pd
 import requests
+import random
 st.title("Pokemon Explorer!")
+def get_random_pokemon():
+    poke_number = random.randint(1, 151)  # Generate a random Pokemon number between 1 and 150
+    url = f'https://pokeapi.co/api/v2/pokemon/{poke_number}/'
+    response = requests.get(url)
+    pokemon = response.json()
+    return pokemon['name'], pokemon['height'], pokemon['weight'], len(pokemon['moves']), pokemon['sprites'], pokemon['cries']
+
 
 def get_details(poke_number):
 	try:
@@ -28,8 +36,14 @@ graph = sns.barplot(data = height_data,
 				   x= 'Pokemon',
 				   y = 'Heights',
 				   palette = colours)
-
-
+if st.button('Show Random Pokemon'):
+    name, height, weight, moves, images, sound = get_random_pokemon()
+    st.write(f'Name: {name}')
+    st.write(f'Height: {height}')
+    st.write(f'Weight: {weight}')
+    st.write(f'Move Count: {moves}')
+    st.image(images['other']['home']['front_default'])
+    st.audio(sound['latest'], format='audio/ogg')
 st.write(f'Name: {name}')
 st.write(f'Height: {height}')
 st.write(f'Weight: {weight}')
